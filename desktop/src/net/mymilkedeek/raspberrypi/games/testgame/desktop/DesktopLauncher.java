1a0fb3808437a5fe23293935538a123f92bbe662
Michael Demey <michael.demey@itextpdf.com>
2017-02-17 21:16:32 +0100

    initial commit pre-raspberry pi configurations

---
+++ desktop/src/net/mymilkedeek/raspberrypi/games/testgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.mymilkedeek.raspberrypi.games.testgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.mymilkedeek.raspberrypi.games.testgame.GameInstance;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameInstance(), config);
+	}
+}
